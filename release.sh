682f41623a6ea15d0c488e9c9eaaa4bc82df83ad
mx3L <mx3ldev@gmail.com>
2013-12-17 16:49:15 +0100

    updated release script

--- release.sh
+++ release.sh
@@ -2,8 +2,30 @@
 
 D=$(pushd $(dirname $0) &> /dev/null; pwd; popd &> /dev/null)
 cd ${D}/custom
-${D}/custom/release.sh $1
+${D}/custom/release.sh
 cd ${D}/dmd-czech
-${D}/dmd-czech/release.sh $1
+${D}/dmd-czech/release.sh
 cd ${D}/xbmc-doplnky
-${D}/xbmc-doplnky/release.sh $1
+${D}/xbmc-doplnky/release.sh
+cd ${D}
+if [ "$1" != "force" ]; 
+then
+	git reset HEAD ${D}/custom/repo
+	git reset HEAD ${D}/dmd-czech/repo
+	git reset HEAD ${D}/xbmc-doplnky/repo
+	git checkout -- ${D}/custom/repo
+	git checkout -- ${D}/dmd-czech/repo
+	git checkout -- ${D}/xbmc-doplnky/repo
+	git add ${D}/custom/repo
+	git add ${D}/custom/addons.xml
+	git add ${D}/custom/addons.md5.xml
+	git add ${D}/xbmc-doplnky/repo
+	git add ${D}/xbmc-doplnky/addons.xml
+	git add ${D}/xbmc-doplnky/addons.md5.xml
+	git add ${D}/dmd-czech/repo
+	git add ${D}/dmd-czech/addons.xml
+	git add ${D}/dmd-czech/addons.md5.xml
+fi
+exit 0
+
+
