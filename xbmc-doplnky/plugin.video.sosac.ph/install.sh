20ce26e0b4fb90f39afc6e3b97cbc36c388172ae
mtester270 <mtester270@gmail.com>
2017-11-07 14:33:37 +0100

    - remove unnecessary file in plugin

--- xbmc-doplnky/plugin.video.sosac.ph/install.sh
+++
@@ -1,7 +0,0 @@
-#/bin/sh
-# do NOT use this script from XBMC addons directory, it is intented for development only
-DESTDIR=~/.kodi/addons/plugin.video.sosac.ph
-
-rm -rf ${DESTDIR}
-mkdir -p ${DESTDIR}
-cp -a * ${DESTDIR}
