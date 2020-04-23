5a5d4b498c4dc918399f11a5b4d8c2f6d0ca9c74
mx3L <mx3Lmail@gmail.com>
2020-04-23 21:51:55 +0200

    make release update

--- make_release.sh
+++ make_release.sh
@@ -2,12 +2,15 @@
 
 D=$(pushd $(dirname $0) &> /dev/null; pwd; popd &> /dev/null)
 
+./release.sh
+
 git reset HEAD ${D}/repo
 git checkout -- ${D}/repo
 git add ${D}/repo
 git add ${D}/addons.xml
 git add ${D}/addons.xml.md5
 
+git commit -m 'release'
 git rev-parse HEAD > commit
 git add commit
-git commit -m 'release'
+git commit -m 'set commit to current release'
