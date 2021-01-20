c9eca1be6444ff7c004dc602ef4281e1ee511232
misanov <annonce@prolidi.cz>
2021-01-20 21:23:57 +0100

    OrangeTV - channel display fix, default channel order

--- plugin.video.orangetv/default.py
+++ plugin.video.orangetv/default.py
@@ -36,7 +36,7 @@ getquality = "1080p"
 otvusr = addon.getSetting('orangetvuser')
 otvpwd = addon.getSetting('orangetvpwd')
 _deviceid = addon.getSetting('deviceid')
-_quality = 'PC'
+_quality = 'MOBILE'
 try:
 	with open(profile + "/epg.dat", "r") as file:
 		epgcache = json.load(file)
@@ -78,7 +78,7 @@ def _log(message):
 	   pass
 
 class LiveChannel:
-	def __init__(self, o2tv, channel_key, name, logo_url, weight, quality, timeshift):
+	def __init__(self, o2tv, channel_key, name, logo_url, weight, quality, timeshift, number):
 		self._o2tv = o2tv
 		self.channel_key = channel_key
 		self.name = name
@@ -86,6 +86,8 @@ class LiveChannel:
 		self.logo_url = logo_url
 		self.quality = quality
 		self.timeshift = timeshift
+		self.number = number
+
 
 class ChannelIsNotBroadcastingError(BaseException):
 	pass
@@ -133,7 +135,6 @@ class O2TVGO:
 				}
 		req = requests.post('https://oauth01.gtm.orange.sk/oauth/token', data=data, headers=headers, verify=False)
 		j = req.json()
-#		_log(j)
 		if 'error' in j:
 			error = j['error']
 			if error == 'authentication-failed':
@@ -164,7 +165,6 @@ class O2TVGO:
 		params = {"deviceId": did}
 		req = requests.get('https://app01.gtm.orange.sk/sws/subscription/settings/remove-device.json', params=params, headers=headers, cookies=cookies)
 		j = req.json()
-#		_log(j)
 
 	def refresh_configuration(self):
 		if not self.access_token:
@@ -218,6 +218,7 @@ class O2TVGO:
 			for channel_id, item in items.iteritems():
 				if channel_id in purchased_channels:
 					live = item['liveTvPlayable']
+					live = True # reseni nekterych nezobrazovanych kanalu?
 					if item['timeShiftDuration']:
 						timeshift = int(item['timeShiftDuration'])/60/24	# pocet dni zpetneho prehravani
 					if live:
@@ -228,7 +229,7 @@ class O2TVGO:
 						name = _to_string(item['channelName'])
 						weight = item['weight']
 						self._live_channels[channel_key] = LiveChannel(
-							self, channel_key, name, logo, weight, quality, timeshift)
+							self, channel_key, name, logo, weight, quality, timeshift, item['channelNumber'])
 			done = False
 			offset = 0
 
@@ -254,7 +255,6 @@ class O2TVGO:
 			params = {"channelKey": ch, "fromTimestamp": fromts, "imageSize": "LARGE", "language": "ces", "offer": self.offer, "toTimestamp": tots}
 			req = requests.get('https://app01.gtm.orange.sk/sws/server/tv/channel-programs.json', params=params, headers=headers, cookies=cookies)
 			j = req.json()
-#			_log(json.dumps(j))
 			epgcache[ch] = []
 			for one in j:
 				title = _to_string(one["name"]) + " - " + datetime.datetime.fromtimestamp(one["startTimestamp"]/1000).strftime('%H:%M') + "-" + datetime.datetime.fromtimestamp(one["endTimestamp"]/1000).strftime('%H:%M')
@@ -290,6 +290,7 @@ nopurch_error = 'NoPurchasedServiceError'
 def OBSAH():
 	addDir("Naživo", 'live', 1, None, infoLabels={'plot':"Prvýkrát sa vďaka kešovania EPG načíta dlhšiu dobu, potom bude už načítať rýchlejšie podľa času v Nastavenie, ktorý si môžete zmeniť (defaultne 24 hodín)."})
 	addDir("Archív", 'archiv', 2, None, infoLabels={'plot':"Tu nájdete spätné prehrávanie vašich kanálov, pokiaľ máte zaplatenú službu archívu."})
+#	addDir("Playlist", 'playlist', 5, None, infoLabels={'plot':"Tímto sa vytvorí orangetv.pls v tmp pro bouguet."})
 	if addon.getSetting('showdevices')=='true':
 		addDir("Zariadenia", 'devices', 9, None, infoLabels={'plot':"Tu si môžete zobraziť a prípadne vymazať/odregistrovať zbytočná zariadenia, aby ste sa mohli znova inde prihlásiť."})
 
@@ -308,7 +309,7 @@ def DEVICEREMOVE(did):
 def ARCHIV():
 	o2tv = O2TVGO(_deviceid, otvusr, otvpwd, _quality, None)
 	channels = o2tv.live_channels()
-	channels_sorted = sorted(channels.values(), key=lambda _channel: _channel.weight)
+	channels_sorted = sorted(channels.values(), key=lambda _channel: _channel.number)
 	for channel in channels_sorted:
 		if channel.timeshift > 0:
 			tsd = channel.timeshift
@@ -338,7 +339,7 @@ def ARCHIVVIDEOS(url):
 def LIVE():
 	o2tv = O2TVGO(_deviceid, otvusr, otvpwd, _quality, None)
 	channels = o2tv.live_channels()
-	channels_sorted = sorted(channels.values(), key=lambda _channel: _channel.weight)
+	channels_sorted = sorted(channels.values(), key=lambda _channel: _channel.number)
 	for channel in channels_sorted:
 		if addon.getSetting('showliveepg')=='true':
 			epg=o2tv.getChannelPrograms(channel.channel_key)
@@ -347,6 +348,56 @@ def LIVE():
 			addDir(_to_string(channel.name),channel.channel_key+"|||",8,channel.logo_url,1)
 	with open(profile + '/epg.dat', 'w') as file:
 		json.dump(epgcache, file)
+
+def PLAYLIST():
+	playlistFHD = "#NAME Orange TV FHD\n"
+	playlist = "#NAME Orange TV\n"
+	count = 0
+	o2tv = O2TVGO(_deviceid, otvusr, otvpwd, _quality, None)
+	channels = o2tv.live_channels()
+	channels_sorted = sorted(channels.values(), key=lambda _channel: _channel.number)
+	if not o2tv.access_token:
+		o2tv.refresh_access_token()
+	if not o2tv.subscription_code:
+		o2tv.refresh_configuration()
+	for channel in channels_sorted:
+		print(channel.channel_key)
+		params = {"serviceType": "LIVE_TV", "subscriptionCode": o2tv.subscription_code,	"channelKey": channel.channel_key, "deviceType": _quality }
+		headers = _COMMON_HEADERS
+		cookies = {"access_token": o2tv.access_token, "deviceId": _deviceid}
+		req = requests.get('http://app01.gtm.orange.sk/sws/server/streaming/uris.json', params=params, headers=headers, cookies=cookies)
+		uris = req.json()
+		try:
+			result = []
+			r = requests.get(uris['uris'][0]['uri'], headers=_COMMON_HEADERS).text
+			for m in re.finditer('#EXT-X-STREAM-INF:PROGRAM-ID=\d+,BANDWIDTH=(?P<bandwidth>\d+),AUDIO="\d+"\s(?P<chunklist>[^\s]+)', r, re.DOTALL):
+				bandwidth = int(m.group('bandwidth'))
+				quality = ""
+				if bandwidth < 2000000:
+					quality = "480p"
+				elif bandwidth < 3000000:
+					quality = "576p"
+				elif bandwidth < 6000000:
+					quality = "720p"
+				else:
+					quality = "1080p"
+				url = m.group('chunklist')
+				if quality == "1080p":
+					playlistFHD += "#SERVICE 4097:0:1:"+str(count)+":0:0:0:0:0:0:"+url.replace(":","%3a")+": "+_to_string(channel.name)+"\n#DESCRIPTION "+_to_string(channel.name)+"\n"
+					continue
+				result.append({"url":url,"title":'['+quality+'] '+name})
+			for one in reversed(result):
+				playlist += "#SERVICE 4097:0:1:"+str(count)+":0:0:0:0:0:0:"+one['url'].replace(":","%3a")+": "+_to_string(channel.name)+"\n#DESCRIPTION "+_to_string(channel.name)+"\n"
+				break
+			count=count+1
+		except:
+			pass
+	f = open(os.path.join(config.plugins.archivCZSK.logPath.getValue(),'orangetvFHD.pls'), 'w')
+	f.write(playlistFHD)
+	f.close()
+	f = open(os.path.join(config.plugins.archivCZSK.logPath.getValue(),'orangetv.pls'), 'w')
+	f.write(playlist)
+	f.close()
 
 def VIDEOLINK(name, url):
 	channel_key,pid,fts,tts = url.split("|")
@@ -366,47 +417,36 @@ def VIDEOLINK(name, url):
 				"channelKey": channel_key,
 				"deviceType": _quality,
 				"fromTimestamp": fts,
-				"toTimestamp": tts,
-				"streamingProtocol": "HLS"}
+				"toTimestamp": tts
+			}
 		else:
 			params = {"serviceType": "LIVE_TV",
 				"subscriptionCode": subscription_code,
 				"channelKey": channel_key,
-				"deviceType": _quality,
-				"streamingProtocol": "HLS"}
+				"deviceType": _quality
+			}
 		headers = _COMMON_HEADERS
 		cookies = {"access_token": access_token, "deviceId": _deviceid}
 		req = requests.get('http://app01.gtm.orange.sk/sws/server/streaming/uris.json', params=params, headers=headers, cookies=cookies)
 		json_data = req.json()
-#		_log(channel_key)
-#		_log(json_data)
 		access_token = None
 		if 'statusMessage' in json_data:
 			status = json_data['statusMessage']
 			if status == 'bad-credentials':
 				access_token = o2tv.refresh_access_token()
-#			elif status == 'channel.not-found':
-#				raise ChannelIsNotBroadcastingError()
 			else:
 				client.showInfo("Err: "+status)
-#				raise Exception(status)
 		else:
-			# Pavuucek: Pokus o vynucení HD kvality
 			playlist = ""
-			# pro kvalitu STB nebo PC se pokusíme vybrat HD adresu.
-			# když není k dispozici, tak první v seznamu
 			for uris in json_data["uris"]:
 				if o2tv.quality == "STB" or o2tv.quality == "PC":
 					if uris["resolution"] == "HD" and playlist == "":
 						playlist = uris["uri"]
 				else:
-					# pro ostatní vracíme SD adresu
 					if uris["resolution"] == "SD" and playlist == "":
 						playlist = uris["uri"]
-			# playlist nebyl přiřazený, takže první adresa v seznamu
 			if playlist == "":
 				playlist = json_data["uris"][0]["uri"]
-	# stahneme a zpracujeme playlist
 	result = []
 	r = requests.get(playlist, headers=_COMMON_HEADERS).text
 	for m in re.finditer('#EXT-X-STREAM-INF:PROGRAM-ID=\d+,BANDWIDTH=(?P<bandwidth>\d+),AUDIO="\d+"\s(?P<chunklist>[^\s]+)', r, re.DOTALL):
@@ -476,6 +516,8 @@ elif mode==3:
 	ARCHIVDAYS(url)
 elif mode==4:
 	ARCHIVVIDEOS(url)
+elif mode==5:
+	PLAYLIST()
 elif mode==9:
 	DEVICES()
 elif mode==8:
